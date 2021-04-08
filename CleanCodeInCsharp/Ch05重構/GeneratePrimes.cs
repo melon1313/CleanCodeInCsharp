0984a8cb7589ae2272da372c119f5d4332aca843
melon1313 <10212130@gm.nttu.edu.tw>
2021-04-08 09:22:42 +0800

    產生質數-初始化

--- CleanCodeInCsharp/Ch05重構/GeneratePrimes.cs
+++ CleanCodeInCsharp/Ch05重構/GeneratePrimes.cs
@@ -7,5 +7,9 @@ namespace CleanCodeInCsharp.Ch05重構
 {
     public class GeneratePrimes
     {
+        public static int[] GeneratePrimeNumbers(int maxValue)
+        {
+            return null;
+        }
     }
 }
